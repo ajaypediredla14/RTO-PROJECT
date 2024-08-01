920a6a780f554a329306fad280653aedeab7e367
ajay pediredla <ajay.pediredla.1818@gmail.com>
2024-08-01 11:44:06 +0530

    seperated env

--- src/model/Database.java
+++ src/model/Database.java
@@ -3,11 +3,12 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import credentials.db;
 
 public class Database {
-    private static final String URL = "jdbc:mysql://localhost:3306/rto_db";
-    private static final String USER = "system";
-    private static final String PASSWORD = "root";
+    private static final String URL = db.URL;
+    private static final String USER = db.USER;
+    private static final String PASSWORD = db.PASSWORD;
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
