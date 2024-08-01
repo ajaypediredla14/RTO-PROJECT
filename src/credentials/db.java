64429ac401d02ca7ed5a890efe6321112ca29de9
ajay pediredla <ajay.pediredla.1818@gmail.com>
2024-08-01 11:46:00 +0530

    seperated env

--- src/credentials/db.java
+++
@@ -1,7 +0,0 @@
-package credentials;
-
-public class db {
-    public static String URL = "jdbc:mysql://localhost:3306/rto_db";
-    public static String USER = "system";
-    public static String PASSWORD = "root";
-}
